b7e5b5cd740c49f5dd0fef8057eef0f95749a106
Jerome Paulos <64276359+jxxe@users.noreply.github.com>
2020-08-09 23:35:39 -0700

    Update color-shade.js

--- color-shade.js
+++ color-shade.js
@@ -1,7 +1,7 @@
 function getColor( percentage, start, end ) {
 
     return 'rgb(' + end.map( (channel, index) => {
-        return Math.round( channel + percentage * ( start[index] - channel ) );
+        return channel + percentage * ( start[index] - channel ) | 0;
     }).join(',') + ')';
 
 }
