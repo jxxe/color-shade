523c87b71a567f982d89fd80fb9035faac416e85
Jerome Paulos <64276359+jxxe@users.noreply.github.com>
2020-08-10 13:36:06 -0700

    Update color-shade.js

--- color-shade.js
+++ color-shade.js
@@ -1,7 +1,12 @@
+// This is not the unminified version of the minified version.
+// This one is a bit more understandable and still super small, even unminified.
+
 function getColor( percentage, start, end ) {
 
-    return 'rgb(' + end.map( (channel, index) => {
-        return channel + percentage * ( start[index] - channel ) | 0;
-    }).join(',') + ')';
+    var result = end.map( (channel, index) => {
+        return Math.round( channel + percentage * ( start[index] - channel ) );
+    } ).join(',');
+
+    return 'rgb(' + result + ')';
 
 }
