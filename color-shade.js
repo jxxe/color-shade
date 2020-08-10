0e81f20747679bc3f233c436284932f9fbf9f683
Jerome Paulos <64276359+jxxe@users.noreply.github.com>
2020-08-09 17:39:05 -0700

    Update color-shade.js

--- color-shade.js
+++ color-shade.js
@@ -1,23 +1,7 @@
-function getColor( percentage, startColor, endColor ) {
-    
-    var start = {
-        r: startColor[0],
-        g: startColor[1],
-        b: startColor[2]
-    }
-    
-    var end = {
-        r: endColor[0],
-        g: endColor[1],
-        b: endColor[2]
-    }
-    
-    var result = {
-        r: Math.round( start.r + percentage * (end.r - start.r) ),
-        g: Math.round( start.g + percentage * (end.g - start.g) ),
-        b: Math.round( start.b + percentage * (end.b - start.b) )
-    }
-    
-    return 'rgb(' + result.r + ',' + result.g + ',' + result.b + ')';
+function getColor( percentage, start, end ) {
+
+    return start.map( (channel, index) => {
+        return channel + percentage * ( end[index] - channel );
+    })
 
 }
